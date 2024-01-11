df83dba88b54560c8af3721bda27a2f44ecd82fe
叶沃钧 <yewojun@okaygis.com>
2024-01-11 17:48:02 +0800

    本地认证提交

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { compareSync } from 'bcryptjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { IStrategyOptions, Strategy } from 'passport-local';
+
+export class LocalStorage extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {
+    super({
+      usernameField: 'username',
+      passwordField: 'password',
+    } as IStrategyOptions);
+  }
+
+  async validate(username: string, password: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where('user.username=:username', { username })
+      .getOne();
+
+    if (!user) {
+      throw new BadRequestException('用户名不正确！');
+    }
+
+    if (!compareSync(password, user.password)) {
+      throw new BadRequestException('密码错误！');
+    }
+
+    return user;
+  }
+}
